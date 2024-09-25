c2eb23c0175d4a41ca0a70cfc819704e8259357b
QuangNV23062004 <nguyenquang23062004@gmail.com>
2024-09-25 08:27:38 +0700

    restyle

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,6 +12,7 @@ import {
 import api from "../../configs/axios";
 import { useNavigate } from "react-router-dom";
 import "./home.css"; // Import the CSS file
+import { EyeOutlined, LikeOutlined } from "@ant-design/icons";
 
 const { Content, Sider } = Layout;
 const { Title, Text } = Typography;
@@ -36,7 +37,11 @@ const HomePage = () => {
   }
   
   const [streams, setStreams] = useState<Stream[]>([]);
-
+  const ArraySize = (arr)=>{
+    if(!arr){
+      return 0;
+    }else return arr.length;
+  }
   useEffect(() => {
     const fetchStreams = async () => {
       try {
@@ -88,30 +93,37 @@ const HomePage = () => {
         />
       </Sider>
 
-      {/* Main Content Section */}
-      <Layout className="sider">
-        <Content style={{ marginBottom: "32px" }}>
+      {/* Main Content Section */} 
+      <Layout className="sider" >
+        <h2 className="title-center" ><span style={{borderBottom: "4px solid rgba(145, 71, 255, 1)"}}>TOP STREAM</span> </h2>
+        <Content style={{}}>
           {topLikedStream ? (
             <div className="top-liked-container">
+              
               <img
                 src={topLikedStream.thumbnailUrl}
                 alt={topLikedStream.title}
                 className="top-liked-image"
               />
-              <div className="top-liked-banner">Top likes</div>
+              <div className="top-liked-banner">Trendy</div>
+              <div className="top-liked-banner2">{topLikedStream.categories[0]}</div>
               <div className="top-liked-bottom">
                 <div className="top-liked-details">
                   <img
                     src={topLikedStream.userId.avatarUrl}
                     alt={`${topLikedStream.userId.name}'s avatar`}
                     className="top-liked-avatar"
+                    onClick={()=>{navigate(`profile/${topLikedStream.userId._id}`)}}
+                    style={{cursor: "pointer"}}
                   />
                   <div style={{ flex: 1, textAlign: "left" }}>
-                    <p className="top-liked-title">{topLikedStream.title}</p>
-                    <p className="top-liked-username">
-                      Stream by: {topLikedStream.userId.name}
+                    <Row className="space-between"><p className="top-liked-title">{topLikedStream.title}</p>
+                    <p className="top-liked-username">{ArraySize(topLikedStream.likeBy)} <LikeOutlined /></p></Row>
+                    <Row className="space-between"><p className="top-liked-username">
+                      Stream by: <span onClick={()=>{navigate(`profile/${topLikedStream.userId._id}`)}} style={{fontWeight: 800, cursor: "pointer"}}>{topLikedStream.userId.name}</span>
                     </p>
-                    <Button
+                    <p className="top-liked-username">{topLikedStream.currentViewCount} <EyeOutlined /></p> </Row>
+                    <button
                       type="default"
                       ghost
                       className="top-liked-button"
@@ -120,7 +132,7 @@ const HomePage = () => {
                       }}
                     >
                       Go to Stream
-                    </Button>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -136,10 +148,10 @@ const HomePage = () => {
             </div>
           )}
         </Content>
-
+          <hr className="splitter"/>
         {/* Featured Streams Section */}
         <Content>
-          <Title level={4} className="title-center">Featured Streams</Title>
+          <h2 className="title-center"><span style={{borderBottom: "4px solid rgba(145, 71, 255, 1)"}}>FEATURED STREAMS</span></h2>
           <Row gutter={[16, 16]}>
             {streams.length > 0 ? (
               streams.map((stream, index) => (
@@ -156,7 +168,7 @@ const HomePage = () => {
                             navigate(`/room/${stream._id}`);
                           }}
                         />
-                        <span className="view-count">{stream.currentViewCount} watching</span>
+                        <span className="view-count">{stream.currentViewCount} <EyeOutlined />  </span>
                         <span className="live-badge">LIVE</span>
                       </div>
                     }
